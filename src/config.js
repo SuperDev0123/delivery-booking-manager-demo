dfb12efd8f586bcabc3f0a7defa35f372490e768
smartworld <smartworld@email.com>
2019-01-07 21:20:16 +0100

    add: prod config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const HTTP_PROTOCOL = 'http';
 export const API_HOST = 'localhost:8000/api'; // Local
 // export const API_HOST = 'ec2-35-161-196-46.us-west-2.compute.amazonaws.com/api'; // Dev
+// export const API_HOST = 'ec2-35-162-92-135.us-west-2.compute.amazonaws.com/api'; // Prod
