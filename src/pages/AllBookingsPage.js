5f754995375675c701b8f6b93ae3d98d8365fdeb
smartworld <smartworld@email.com>
2019-01-24 01:01:49 +0100

    fix: download button issue

--- src/pages/AllBookingsPage.js
+++ src/pages/AllBookingsPage.js
@@ -407,7 +407,8 @@ class AllBookingsPage extends React.Component {
                     const url = window.URL.createObjectURL(new Blob([response.data]));
                     const link = document.createElement('a');
                     link.href = url;
-                    link.setAttribute('download', selectedBookingIds[i]);
+                    // link.setAttribute('download', selectedBookingIds[i] + '.pdf');
+                    link.setAttribute('download', selectedBookingIds[i] + '.txt');
                     document.body.appendChild(link);
                     link.click();
                 });
