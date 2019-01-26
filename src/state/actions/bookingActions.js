f692eac1ef6f39c22199938905faeb029ee61948
smartworld <kimurajuni20@hotmail.com>
2019-01-26 22:02:27 +0100

    1: add input button for visual bookingID or Consignment Number, 2: next, prev -and first last buttons to navigate before and after booking

--- src/state/actions/bookingActions.js
+++ src/state/actions/bookingActions.js
@@ -1,9 +1,17 @@
-import { SET_BOOKINGS, FAILED_GET_BOOKINGS, SET_BOOKING, FAILED_UPDATE_BOOKING, SET_MAPPEDBOOKINGS, SET_USER_DATE_FILTER_FIELD, FAILED_GET_USER_DATE_FILTER_FIELD } from '../constants/bookingConstants';
+import { SET_BOOKING_WITH_FILTER, SET_BOOKINGS, FAILED_GET_BOOKINGS, SET_BOOKING, FAILED_UPDATE_BOOKING, SET_MAPPEDBOOKINGS, SET_USER_DATE_FILTER_FIELD, FAILED_GET_USER_DATE_FILTER_FIELD } from '../constants/bookingConstants';
 
 export function setBookings(bookings) {
     return {
         type: SET_BOOKINGS,
         bookings
+    };
+}
+
+export function setBookingFilter(data) {
+    console.log('@!' + data);
+    return {
+        type: SET_BOOKING_WITH_FILTER,
+        booking: data.booking
     };
 }
 
