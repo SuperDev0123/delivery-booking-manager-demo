2ddb65d0f69d5f7cc7fccbdb65d91c34b33a602c
smartworld <smartworld@email.com>
2019-02-20 01:38:44 +0300

    fix: excel api protocal

--- src/state/services/bookingService.js
+++ src/state/services/bookingService.js
@@ -242,7 +242,7 @@ export const stOrder = () => {
 export const getExcel = () => {
     const token = localStorage.getItem('token');
     const options = {
-        method: 'post',
+        method: 'get',
         headers: { 'Content-Type': 'application/json', 'Authorization': 'JWT ' + token },
         url: `${HTTP_PROTOCOL}://${API_HOST}/excel/`
     };
