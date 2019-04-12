ec045b7881bbf389c6a7961f7aeeee7c1a796788
smartworld <smartworld@email.com>
2019-04-12 22:40:25 +0300

    change: config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const HTTP_PROTOCOL = 'http';
 export const API_HOST = 'localhost:8000/api';       // Local Api
 export const STATIC_HOST = 'localhost:8000/static'; // Local Static
-// export const API_HOST = 'ec2-35-161-196-46.us-west-2.compute.amazonaws.com/api';         // Dev Api
-// export const STATIC_HOST = 'ec2-35-161-196-46.us-west-2.compute.amazonaws.com/static';   // Dev Static
-// export const API_HOST = 'ec2-35-162-92-135.us-west-2.compute.amazonaws.com/api';         // Prod Api
-// export const STATIC_HOST = 'ec2-35-162-92-135.us-west-2.compute.amazonaws.com/static';   // Prod Static
+// export const API_HOST = '3.105.62.128/api';         // Dev Api
+// export const STATIC_HOST = '3.105.62.128/static';   // Dev Static
+// export const API_HOST = '13.55.64.102/api';         // Prod Api
+// export const STATIC_HOST = '13.55.64.102/static';   // Prod Static
