61b7f6d7595dfb4396b81b15af98bcdba83b593c
smartworld <smartworld@email.com>
2019-09-03 16:27:10 +0200

    fix: DME role users can edit LineData even it is booked

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -3805,7 +3805,7 @@ class BookingPage extends Component {
                                                 <Button 
                                                     className="edit-lld-btn btn-primary" 
                                                     onClick={this.toggleShowLineSlider} 
-                                                    disabled={!isBookingSelected || (isBookedBooking && clientname === 'dme')}
+                                                    disabled={!isBookingSelected || (isBookedBooking && clientname !== 'dme')}
                                                 >
                                                     Edit
                                                 </Button>
