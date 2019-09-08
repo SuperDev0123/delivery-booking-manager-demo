04c1370585e257c1f56c49e82143091cb116a395
smartworld <smartworld@email.com>
2019-09-08 17:22:04 +0200

    fix: get all Comms after one is created or deleted

--- src/state/reducers/commReducer.js
+++ src/state/reducers/commReducer.js
@@ -40,10 +40,6 @@ export const CommReducer = (state = defaultState, { type, errorMessage, comms, c
                 errorMessage: errorMessage 
             };
         case SUCCESS_UPDATE_COMM:
-            return { 
-                ...state, 
-                comm: comm,
-            };
         case SUCCESS_DELETE_COMM:
             return { 
                 ...state, 
