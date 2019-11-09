bb08efa4d4fa7855035e4198271ee88c67c7a579
smartworld <smartworld@email.com>
2019-11-09 15:03:41 +0100

    mod: pageItemCnt options - 10, 50, 100, 200

--- src/pages/AllBookingsPage.js
+++ src/pages/AllBookingsPage.js
@@ -2266,8 +2266,9 @@ class AllBookingsPage extends React.Component {
                                                 </label>
                                                 <select value={this.state.pageItemCnt} onChange={(e) => this.onPageItemCntChange(e)}>
                                                     <option value="10">10</option>
-                                                    <option value="20">20</option>
                                                     <option value="50">50</option>
+                                                    <option value="100">100</option>
+                                                    <option value="200">200</option>
                                                 </select>
                                                 <CustomPagination 
                                                     onClickPagination={(type) => this.onClickPagination(type)}
