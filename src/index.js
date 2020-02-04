bfee61e1b33fc4cda5f00067f7c1477f0288e19f
royalone <greatroyalone@outlook.com>
2020-02-04 07:58:22 -0800

    disable bugsnag on Dev

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ import 'babel-polyfill';
 import bugsnag from '@bugsnag/js';
 import bugsnagReact from '@bugsnag/plugin-react';
 
-const bugsnagClient = bugsnag('30538436eab90ba75465f678a49e4390');
-bugsnagClient.use(bugsnagReact, React);
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    ReactDOM.render( <App /> , document.getElementById('app'));
+} 
+else {
+    const bugsnagClient = bugsnag('30538436eab90ba75465f678a49e4390');
+    bugsnagClient.use(bugsnagReact, React);
+    const ErrorBoundary = bugsnagClient.getPlugin('react');
+    ReactDOM.render(<ErrorBoundary> <App /> </ErrorBoundary>, document.getElementById('app'));
+}
 
 
-const ErrorBoundary = bugsnagClient.getPlugin('react');
-ReactDOM.render(<ErrorBoundary> <App /> </ErrorBoundary>, document.getElementById('app'));
 
 /* bugsnagClient.notify(new Error('Test error')); */
