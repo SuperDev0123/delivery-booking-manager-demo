2762aeae6933a1644c71414de7cde189750ac566
royalone <greatroyalone@outlook.com>
2020-05-08 03:17:15 +0800

    implement search and loading bar

--- src/state/reducers/fileReducer.js
+++ src/state/reducers/fileReducer.js
@@ -1,23 +1,32 @@
 import {
     SUCCESS_GET_FILES,
     FAILED_GET_FILES,
+    RESET_GET_FILES,
 } from '../constants/fileConstants';
 
 const defaultState = {
     files: [],
+    loadingFiles: false,
     errorMessage: '',
 };
 
 export const FileReducer = (state = defaultState, { type, payload }) => {
     switch (type) {
+        case RESET_GET_FILES:
+            return {
+                ...state,
+                loadingFiles: true,
+            };
         case SUCCESS_GET_FILES:
             return {
                 ...state,
                 files: payload,
+                loadingFiles: false,
             };
         case FAILED_GET_FILES:
             return {
                 ...state,
+                loadingFiles: false,
                 errorMessage: payload,
             };
         default:
