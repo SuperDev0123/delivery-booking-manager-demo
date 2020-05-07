6e45c3106081e8b651b479fe148ab429fb426eef
royalone <greatroyalone@outlook.com>
2020-05-08 03:17:15 +0800

    implement search and loading bar

--- src/state/actions/fileActions.js
+++ src/state/actions/fileActions.js
@@ -1,6 +1,7 @@
 import {
     SUCCESS_GET_FILES,
     FAILED_GET_FILES,
+    RESET_GET_FILES,
 } from '../constants/fileConstants';
 
 export function successGetFiles(data) {
@@ -14,5 +15,11 @@ export function failedGetFiles(error) {
     return {
         type: FAILED_GET_FILES,
         payload: 'Unable to get files records. Error:' + error,
+    };
+}
+
+export function resetGetFiles() {
+    return {
+        type: RESET_GET_FILES,
     };
 }
