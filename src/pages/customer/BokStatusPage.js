ef801a2d4b056123aaad5afd9ea43f1f4a11e4d9
smartworld-dm <smartworld@email.com>
2021-06-15 03:09:44 +0200

    feat: detailPage url on CustomerStatus page

--- src/pages/customer/BokStatusPage.js
+++ src/pages/customer/BokStatusPage.js
@@ -138,7 +138,10 @@ class BokStatusPage extends Component {
                             </div>
                         }
                         <div className="status-chart-detail">
-                            {booking && booking.hasOwnProperty('b_bookingID_Visual') && <p><strong>DME Booking Number: </strong>{booking.b_bookingID_Visual}</p>}
+                            {(booking && booking.hasOwnProperty('b_bookingID_Visual') && booking['b_bookingID_Visual']) ?
+                                <p><strong>DME Booking Number: </strong><a href={`/booking?bookingid=${booking['uid']}`} rel="noopener noreferrer" target="_blank">{booking.b_bookingID_Visual}</a></p>
+                                : <p><strong>DME Booking Number: </strong>Not available</p>
+                            }
                             {booking && booking.hasOwnProperty('b_client_order_num') && <p><strong>Client Order Number: </strong>{booking.b_client_order_num}</p>}
                             {booking && booking.hasOwnProperty('b_client_sales_inv_num') && <p><strong>Client Sales Invoice Number: </strong>{booking.b_client_sales_inv_num}</p>}
                             {!status && <p><strong>Status</strong>: Not available</p>}
