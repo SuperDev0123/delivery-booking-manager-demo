6e6ee54daa0dd7b154ee2b8180677f5c304b17a1
smartworld-dm <smartworld@email.com>
2021-08-08 00:03:46 +0200

    mod: config for DNS

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 //-- Local --
 export const HTTP_PROTOCOL = 'http';
-export const API_HOST = 'localhost:8000/api';       									// Local Api
+export const API_HOST = 'localhost:8000/api';       										// Local Api
 export const STATIC_HOST = 'localhost:8000/static'; 										// Local Static
 
 // Zoho
@@ -12,9 +12,9 @@ export const ZOHO_ORG_ID = '7000200810';
 
 //-- DEV --
 // export const HTTP_PROTOCOL = 'https';
-// export const API_HOST = '3.105.62.128/api';                 								// Dev Api
-// export const STATIC_HOST = S3_URL;                          								// Dev Static
+// export const API_HOST = 'dev.api.deliver-me.com.au/api';                 				// Dev Api
 // export const S3_URL = 'dme-dev-api-static-public.s3-ap-southeast-2.amazonaws.com';
+// export const STATIC_HOST = S3_URL;                          								// Dev Static
 
 // Zoho
 // export const ZOHO_REDIRECT_URI = 'https://dev.deliver-me.com.au/zoho';
@@ -25,9 +25,9 @@ export const ZOHO_ORG_ID = '7000200810';
 
 //-- PROD --
 // export const HTTP_PROTOCOL = 'https';
-// export const API_HOST = '13.55.64.102/api';              						// Prod Api
-// export const STATIC_HOST = S3_URL;                       								// Prod Static
+// export const API_HOST = 'prod.api.deliver-me.com.au/api';              					// Prod Api
 // export const S3_URL = 'dme-pod-api-static.s3-ap-southeast-2.amazonaws.com';
+// export const STATIC_HOST = S3_URL;                       								// Prod Static
 
 // Zoho
 // export const ZOHO_REDIRECT_URI = 'https://deliver-me.com.au/zoho';
