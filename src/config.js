af4583dcdf8b071ed3642c291b62ad681fcf644e
smartworld-dm <smartworld@email.com>
2021-08-07 23:10:46 +0200

    mod: config for DNS

--- src/config.js
+++ src/config.js
@@ -1,12 +1,7 @@
-//-- Common --
-
-export const HTTP_PROTOCOL = 'http';
-
-
 //-- Local --
-
-export const API_HOST = 'localhost:8000/api';       // Local Api
-export const STATIC_HOST = 'localhost:8000/static'; // Local Static
+export const HTTP_PROTOCOL = 'http';
+export const API_HOST = 'http://localhost:8000/api';       									// Local Api
+export const STATIC_HOST = 'localhost:8000/static'; 										// Local Static
 
 // Zoho
 export const ZOHO_REDIRECT_URI = 'http://localhost:9000/zoho';
@@ -16,26 +11,26 @@ export const ZOHO_ORG_ID = '7000200810';
 
 
 //-- DEV --
-
+// export const HTTP_PROTOCOL = 'https';
+// export const API_HOST = '3.105.62.128/api';                 								// Dev Api
+// export const STATIC_HOST = S3_URL;                          								// Dev Static
 // export const S3_URL = 'dme-dev-api-static-public.s3-ap-southeast-2.amazonaws.com';
-// export const API_HOST = '3.105.62.128/api';                 // Dev Api
-// export const STATIC_HOST = S3_URL;                          // Dev Static
 
 // Zoho
-// export const ZOHO_REDIRECT_URI = 'http://3.104.30.210/zoho';
+// export const ZOHO_REDIRECT_URI = 'https://dev.deliver-me.com.au/zoho';
 // export const ZOHO_CLIENT_ID = '1000.ZVVPWDMH4SY3ZW6TDUI1011KO4HKNN';
 // export const ZOHO_CLIENT_SECRET = '697d31c1bb2ac8e4f13a369262522cb72b5bd3af30';
 // export const ZOHO_ORG_ID = '7000200810';
 
 
 //-- PROD --
-
+// export const HTTP_PROTOCOL = 'https';
+// export const API_HOST = 'https://13.55.64.102/api';              						// Prod Api
+// export const STATIC_HOST = S3_URL;                       								// Prod Static
 // export const S3_URL = 'dme-pod-api-static.s3-ap-southeast-2.amazonaws.com';
-// export const API_HOST = '13.55.64.102/api';              // Prod Api
-// export const STATIC_HOST = S3_URL;                       // Prod Static
 
 // Zoho
-// export const ZOHO_REDIRECT_URI = 'http://13.55.160.158/zoho';
+// export const ZOHO_REDIRECT_URI = 'https://deliver-me.com.au/zoho';
 // export const ZOHO_CLIENT_ID = '1000.42ANMURFOIIQP8GFPSLVHH63K5EYPN';
 // export const ZOHO_CLIENT_SECRET = 'f306eba52bb0987d8620e0821e0421ffd6995c0b18';
 // export const ZOHO_ORG_ID = '7000200810';
