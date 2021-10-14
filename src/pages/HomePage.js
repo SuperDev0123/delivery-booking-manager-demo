9e81b8eadcbf1ddfbbb026030355540046badee0
smartworld-dm <smartworld@email.com>
2021-10-14 04:49:24 +0200

    mod: landing page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -44,13 +44,10 @@ class HomePage extends Component {
             <section className="theme-bg">
                 <div className="right-pan">
                     <p>Deliver Me</p>
-                    <h5>Your Freight Department - Automated From your Delivery Concierge.. </h5>
-                    <br />
-                    <br />
+                    <h5>Your Freight Department - Automated</h5>
                     <br />
                     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: 10, padding: 30}}>
-                        <h4 style={{fontWeight: 'bold'}}>The Deliver-ME freight department module is a platform that allows our clients to automate their frieght function</h4>
-                        <br />
+                        <h4>The Deliver-ME freight department module is a platform that allows our clients to automate their frieght function</h4>
                         <h4>From organising all requirements needed to optimise the freight logistics function and their customer&apos;s experience</h4>
                         <i className="fas fa-angle-double-down" style={{fontSize: 16, fontWeight: 'bold'}}></i>
                         <h4>to accurate live costing of freight sensitive to the end customer&apos;s needs when selling online or via their internal sales application</h4>
@@ -71,14 +68,14 @@ class HomePage extends Component {
                             <img className="d-block w-100" src={img1} alt="First slide" />
                             <div className="carousel-caption d-none d-md-block">
                                 <h1>Deliver Me</h1>
-                                <h4>Your Freight Department - Automated From your Delivery Concierge</h4>
+                                <h4>Your Freight Department - Automated</h4>
                             </div>
                         </div>
                         <div className="carousel-item">
                             <img className="d-block w-100" src={img2} alt="Second slide" />
                             <div className="carousel-caption d-none d-md-block">
                                 <h1>Deliver Me</h1>
-                                <h4>Your Freight Department - Automated From your Delivery Concierge.. </h4>
+                                <h4>Your Freight Department - Automated</h4>
                             </div>
                         </div>
                         <div className="carousel-item">
