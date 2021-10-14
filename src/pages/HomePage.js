7be586405b116a4a12f97ebf8c700cbe30c25aa8
Daniel Yang <astar1112.rich@outlook.com>
2021-10-14 02:43:57 +0000

    update landing page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -44,7 +44,21 @@ class HomePage extends Component {
             <section className="theme-bg">
                 <div className="right-pan">
                     <p>Deliver Me</p>
-                    <p>Your Delivery Concierge.. </p>
+                    <h5>Your Freight Department - Automated From your Delivery Concierge.. </h5>
+                    <br />
+                    <br />
+                    <br />
+                    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: 10, padding: 30}}>
+                        <h4 style={{fontWeight: 'bold'}}>The Deliver-ME freight department module is a platform that allows our clients to automate their frieght function</h4>
+                        <br />
+                        <h4>From organising all requirements needed to optimise the freight logistics function and their customer&apos;s experience</h4>
+                        <i className="fas fa-angle-double-down" style={{fontSize: 16, fontWeight: 'bold'}}></i>
+                        <h4>to accurate live costing of freight sensitive to the end customer&apos;s needs when selling online or via their internal sales application</h4>
+                        <i className="fas fa-angle-double-down" style={{fontSize: 16, fontWeight: 'bold'}}></i>
+                        <h4>to freight packing, despatch and delivery management</h4>
+                        <i className="fas fa-angle-double-down" style={{fontSize: 16, fontWeight: 'bold'}}></i>
+                        <h4>through to reconciliation and payment of freight bills</h4>
+                    </div>
                 </div>
                 <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
                     <ol className="carousel-indicators">
@@ -57,14 +71,14 @@ class HomePage extends Component {
                             <img className="d-block w-100" src={img1} alt="First slide" />
                             <div className="carousel-caption d-none d-md-block">
                                 <h1>Deliver Me</h1>
-                                <h4>Your Delivery Concierge</h4>
+                                <h4>Your Freight Department - Automated From your Delivery Concierge</h4>
                             </div>
                         </div>
                         <div className="carousel-item">
                             <img className="d-block w-100" src={img2} alt="Second slide" />
                             <div className="carousel-caption d-none d-md-block">
                                 <h1>Deliver Me</h1>
-                                <h4>Your Delivery Concierge.. </h4>
+                                <h4>Your Freight Department - Automated From your Delivery Concierge.. </h4>
                             </div>
                         </div>
                         <div className="carousel-item">
