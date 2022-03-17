85890380f1f64aab4428de45d5d53fc908ba5a22
Daniel <astar1112.rich@outlook.com>
2022-03-17 15:44:44 +0800

    add filter on report page

--- src/pages/report/_ManifestReport.js
+++ src/pages/report/_ManifestReport.js
@@ -13,6 +13,9 @@ import 'react-toastify/dist/ReactToastify.css';
 import { getManifestReport, setAllGetBookingsFilter } from '../../state/services/bookingService';
 // Constants
 import { API_HOST, HTTP_PROTOCOL } from '../../config';
+import { getAllClients } from '../../state/services/clientService';
+import { getAllFPs } from '../../state/services/extraService';
+import { getUser } from '../../state/services/authService';
 
 class ManifestReport extends React.Component {
     constructor(props) {
@@ -21,25 +24,46 @@ class ManifestReport extends React.Component {
         this.state = {
             loadingDownload: false,
             loading: true,
+            fpFilter: '',
+            clientFilter: '',
+            reportList: [],
+            fpOptions: [],
+            clients: [],
         };
     }
 
     static propTypes = {
         history: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired,
         getManifestReport: PropTypes.func.isRequired,
+        getAllClients: PropTypes.func.isRequired,
+        getAllFPs: PropTypes.func.isRequired,
         setAllGetBookingsFilter: PropTypes.func.isRequired,
         reports: PropTypes.array.isRequired,
+        clientname: PropTypes.string.isRequired,
+        allFPs: PropTypes.array.isRequired,
+        clients: PropTypes.array.isRequired,
     };
 
     componentDidMount() {
+        const isLoggedIn = localStorage.getItem('isLoggedIn');
+        const token = localStorage.getItem('token');
+
+        if (isLoggedIn && token && token.length > 0)
+            this.props.getUser(token);
+
         this.props.getManifestReport();
+        this.props.getAllClients();
+        this.props.getAllFPs();
+        
     }
 
     UNSAFE_componentWillReceiveProps(newProps) {
         const { reports } = newProps;
-    
+
         if (reports) {
             this.setState({loading: false, reports});
+            this.onFind(reports);
         }
     }
 
@@ -79,42 +103,71 @@ class ManifestReport extends React.Component {
         this.props.history.push('/allbookings');
     }
 
-    render() {
-        const { reports } = this.props;
-        const { loading } = this.state;
-        var reportList = [];
-
+    onChangeFilterType = (e, filterType) => {
+        this.setState({[filterType]: e.target.value});
+    }
+    
+    onFind = (reports) => {
+        const { clientname } = this.props;
+        const { fpFilter, clientFilter } = this.state;
         if (reports) {
-            reportList = reports.map((report, index) => {
-                return (
-                    <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{moment(report.manifest_date).format('DD MMM YYYY')}</td>
-                        <td>{report.warehouse_name}</td>
-                        <td>{report.freight_provider}</td>
-                        <td>{report.count}</td>
-                        <td>
-                            <button
-                                className="btn btn-primary"
-                                onClick={() => this.onClickDownload(report)}
-                            >
-                                Download
-                            </button>
-                            <button
-                                className="btn btn-primary"
-                                onClick={() => this.onClickViewOnAllBookingsTab(report)}
-                            >
-                                View on Allbookings tab
-                            </button>
-                            <button
-                                className="btn btn-primary"
-                                onClick={() => this.onClickCopy(report)}
-                            >
-                                Copy Booking IDs
-                            </button>
-                        </td>
-                    </tr>
-                );
+            const filteredReports = reports.filter(report => 
+                report.freight_provider.includes(fpFilter) &&
+                clientname == 'dme' &&
+                report.kf_client_id.includes(clientFilter)
+            );
+            if (filteredReports) {
+                let reportList = filteredReports.map((report, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{moment(report.manifest_date).format('DD MMM YYYY')}</td>
+                            <td>{report.warehouse_name}</td>
+                            <td>{report.freight_provider}</td>
+                            <td>{report.count}</td>
+                            <td>
+                                <button
+                                    className="btn btn-primary"
+                                    onClick={() => this.onClickDownload(report)}
+                                >
+                                    Download
+                                </button>
+                                <button
+                                    className="btn btn-primary"
+                                    onClick={() => this.onClickViewOnAllBookingsTab(report)}
+                                >
+                                    View on Allbookings tab
+                                </button>
+                                <button
+                                    className="btn btn-primary"
+                                    onClick={() => this.onClickCopy(report)}
+                                >
+                                    Copy Booking IDs
+                                </button>
+                            </td>
+                        </tr>
+                    );
+                });
+                this.setState({ reportList: reportList});
+            }
+        }
+
+    }
+
+    render() {
+        const { clientname, allFPs, clients } = this.props;
+        const { loading, fpFilter, clientFilter } = this.state;
+        let fpFilterOpts = [];
+        let clientsOpts = [];
+        if (allFPs) {
+            fpFilterOpts = allFPs.map((fp, index) => {
+                return <option value={fp.fp_company_name} key={index}>{fp.fp_company_name}</option>;
+            });
+        }
+
+        if (clients) {
+            clientsOpts = clients.map((client, index) => {
+                return <option value={client.dme_account_num} key={index}>{client.company_name}</option>;
             });
         }
 
@@ -125,21 +178,36 @@ class ManifestReport extends React.Component {
                 text='Loading...'
             >
                 <div className="manifest">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Date</th>
-                                <th>Warehouse</th>
-                                <th>Freight Provider</th>
-                                <th>Count</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {reportList}
-                        </tbody>
-                    </table>
+                    <div className="py-2">
+                        <select value={fpFilter} onChange={(e) => this.onChangeFilterType(e, 'fpFilter')} >
+                            <option value="" selected>All</option>
+                            {fpFilterOpts}
+                        </select>&nbsp;&nbsp;
+                        {clientname == 'dme' &&
+                            <select value={clientFilter} onChange={(e) => this.onChangeFilterType(e, 'clientFilter')} >
+                                <option value="" selected>All</option>
+                                {clientsOpts}
+                            </select>
+                        }
+                        <button type="button" onClick={() => this.onFind(this.props.reports)} className="btn btn-sm btn-success pull-right">Find</button>
+                    </div>
+                    <div className="py-2">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Date</th>
+                                    <th>Warehouse</th>
+                                    <th>Freight Provider</th>
+                                    <th>Count</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.reportList}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
                 <ToastContainer />
@@ -151,12 +219,18 @@ class ManifestReport extends React.Component {
 const mapStateToProps = (state) => {
     return {
         reports: state.booking.manifestReports,
+        clientname: state.auth.clientname,
+        clients: state.client.clients,
+        allFPs: state.extra.allFPs,
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
+        getUser: (token) => dispatch(getUser(token)),
         getManifestReport: () => dispatch(getManifestReport()),
+        getAllClients: () => dispatch(getAllClients()),
+        getAllFPs: () => dispatch(getAllFPs()),
         setAllGetBookingsFilter: (startDate, endDate, clientPK, warehouseId, fpId, pageItemCnt, pageInd, sortField, columnFilters, activeTabInd, simpleSearchKeyword, downloadOption, dmeStatus, multiFindField, multiFindValues, projectName) => dispatch(setAllGetBookingsFilter(startDate, endDate, clientPK, warehouseId, fpId, pageItemCnt, pageInd, sortField, columnFilters, activeTabInd, simpleSearchKeyword, downloadOption, dmeStatus, multiFindField, multiFindValues, projectName)),
     };
 };
