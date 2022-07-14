08b87aad779675da7f4da58946d0b29db3b2c0c7
Daniel <astar1112.rich@outlook.com>
2022-07-15 00:08:57 +0800

    fix style of XLS modal

--- src/components/CommonModals/XLSModal.js
+++ src/components/CommonModals/XLSModal.js
@@ -178,98 +178,100 @@ class XLSModal extends Component {
             <ReactstrapModal isOpen={isShowXLSModal} toggle={() => this.props.toggleXLSModal()} className="xls-modal">
                 <ModalHeader toggle={() => this.props.toggleXLSModal()}>XLS Download</ModalHeader>
                 <ModalBody>
-                    <label>
-                        <p>Report Type: </p>
-                        <select
-                            required 
-                            name="report_type" 
-                            onChange={(e) => this.onInputChange(e, 'report_type')}
-                            value = {report_type ? report_type : ''} >
-                            <option value="" disabled hidden>Select a Report Type</option>
-                            <option value="booking">Booking Report</option>
-                            <option value="booking_line">Booking Line Report</option>
-                            <option value="booking_with_gaps">Booking w/gaps</option>
-                            {clientname === 'dme' && <option value="dme_booking_with_gaps">DME Booking with GAPS report</option>}
-                            <option value="whse">FP/Whse Report</option>
-                            {clientname === 'dme' && <option value="pending_bookings">Pending Bookings report</option>}
-                            {clientname === 'dme' && <option value="booked_bookings">Booked Bookings report</option>}
-                            {clientname === 'dme' && <option value="picked_up_bookings">Picked up Bookings report</option>}
-                            {clientname === 'dme' && <option value="box">Box Bookings report</option>}
-                            {clientname === 'dme' && <option value="futile">Futile Bookings report</option>}
-                            {clientname === 'dme' && <option value="goods_delivered">Goods Delivered report</option>}
-                            {clientname === 'dme' && <option value="goods_sent">Goods Sent report</option>}
-                            <option value="all">All</option>
-                        </select>
-                    </label>
-                    {!useSelected &&
-                        <label>
-                            <p>Client: </p>
+                    <div className='modal-body'>
+                        <div>
+                            <p>Report Type: </p>
                             <select
                                 required 
-                                name="b_client_name" 
-                                onChange={(e) => this.onInputChange(e, 'b_client_name')}
-                                value = {b_client_name ? b_client_name : ''} >
-                                <option value="" disabled hidden>Select a Client</option>
-                                <option value="All">All</option>
-                                {clientList}
+                                name="report_type" 
+                                onChange={(e) => this.onInputChange(e, 'report_type')}
+                                value = {report_type ? report_type : ''} >
+                                <option value="" disabled hidden>Select a Report Type</option>
+                                <option value="booking">Booking Report</option>
+                                <option value="booking_line">Booking Line Report</option>
+                                <option value="booking_with_gaps">Booking w/gaps</option>
+                                {clientname === 'dme' && <option value="dme_booking_with_gaps">DME Booking with GAPS report</option>}
+                                <option value="whse">FP/Whse Report</option>
+                                {clientname === 'dme' && <option value="pending_bookings">Pending Bookings report</option>}
+                                {clientname === 'dme' && <option value="booked_bookings">Booked Bookings report</option>}
+                                {clientname === 'dme' && <option value="picked_up_bookings">Picked up Bookings report</option>}
+                                {clientname === 'dme' && <option value="box">Box Bookings report</option>}
+                                {clientname === 'dme' && <option value="futile">Futile Bookings report</option>}
+                                {clientname === 'dme' && <option value="goods_delivered">Goods Delivered report</option>}
+                                {clientname === 'dme' && <option value="goods_sent">Goods Sent report</option>}
+                                <option value="all">All</option>
                             </select>
-                        </label>
-                    }
-                    {!useSelected &&
-                        <label>
-                            <p>Freight Provider: </p>
-                            <select
-                                required 
-                                name="vx_freight_provider" 
-                                onChange={(e) => this.onInputChange(e, 'vx_freight_provider')}
-                                value = {vx_freight_provider ? vx_freight_provider : ''} >
-                                <option value="" disabled hidden>Select a FP</option>
-                                <option value="All">All</option>
-                                {fpList}
-                            </select>
-                        </label>
-                    }
-                    <label>
-                        <p>Use selected: </p>
-                        <input
-                            type="checkbox"
-                            name="useSelected"
-                            className="checkbox"
-                            checked={useSelected}
-                            onChange={(e) => this.onInputChange(e, 'useSelected')}
-                        />
-                    </label>
-                    {!useSelected &&
-                        <label>
-                            <p>Start Date: </p>
-                            <DatePicker
-                                selected={startDate}
-                                onChange={(e) => this.onDateChange(e, 'startDate')}
-                                dateFormat="dd MMM yyyy"
-                                disabled={useSelected ? 'disabled' : null}
+                        </div>
+                        {!useSelected &&
+                            <div>
+                                <p>Client: </p>
+                                <select
+                                    required 
+                                    name="b_client_name" 
+                                    onChange={(e) => this.onInputChange(e, 'b_client_name')}
+                                    value = {b_client_name ? b_client_name : ''} >
+                                    <option value="" disabled hidden>Select a Client</option>
+                                    <option value="All">All</option>
+                                    {clientList}
+                                </select>
+                            </div>
+                        }
+                        {!useSelected &&
+                            <div>
+                                <p>Freight Provider: </p>
+                                <select
+                                    required 
+                                    name="vx_freight_provider" 
+                                    onChange={(e) => this.onInputChange(e, 'vx_freight_provider')}
+                                    value = {vx_freight_provider ? vx_freight_provider : ''} >
+                                    <option value="" disabled hidden>Select a FP</option>
+                                    <option value="All">All</option>
+                                    {fpList}
+                                </select>
+                            </div>
+                        }
+                        <div>
+                            <p>Use selected: </p>
+                            <input
+                                type="checkbox"
+                                name="useSelected"
+                                className="checkbox"
+                                checked={useSelected}
+                                onChange={(e) => this.onInputChange(e, 'useSelected')}
                             />
-                        </label>
-                    }
-                    {!useSelected &&
-                        <label>
-                            <p>End Date: </p>
-                            <DatePicker
-                                selected={endDate}
-                                onChange={(e) => this.onDateChange(e, 'endDate')}
-                                dateFormat="dd MMM yyyy"
-                                disabled={useSelected ? 'disabled' : null}
-                            />
-                        </label>
-                    }
-                    <label>
-                        <p>Email address: </p>
-                        <input type="text" placeholder="Email to send xls" name="emailAddr" value={emailAddr} onChange={(e) => this.onInputChange(e, 'email')} />
-                    </label>
-                    <label>
-                        <p>Show Field Names: </p>
-                        <input type="checkbox" name="showFieldName" className="checkbox" checked={showFieldName} onChange={(e) => this.onInputChange(e, 'showFieldName')} />
-                    </label>
-                    <p className="red">{errorMessage}</p>
+                        </div>
+                        {!useSelected &&
+                            <div>
+                                <p>Start Date: </p>
+                                <DatePicker
+                                    selected={startDate}
+                                    onChange={(e) => this.onDateChange(e, 'startDate')}
+                                    dateFormat="dd MMM yyyy"
+                                    disabled={useSelected ? 'disabled' : null}
+                                />
+                            </div>
+                        }
+                        {!useSelected &&
+                            <div>
+                                <p>End Date: </p>
+                                <DatePicker
+                                    selected={endDate}
+                                    onChange={(e) => this.onDateChange(e, 'endDate')}
+                                    dateFormat="dd MMM yyyy"
+                                    disabled={useSelected ? 'disabled' : null}
+                                />
+                            </div>
+                        }
+                        <div>
+                            <p>Email address: </p>
+                            <input type="text" placeholder="Email to send xls" name="emailAddr" value={emailAddr} onChange={(e) => this.onInputChange(e, 'email')} />
+                        </div>
+                        <div>
+                            <p>Show Field Names: </p>
+                            <input type="checkbox" name="showFieldName" className="checkbox" checked={showFieldName} onChange={(e) => this.onInputChange(e, 'showFieldName')} />
+                        </div>
+                        <p className="red">{errorMessage}</p>
+                    </div>
                 </ModalBody>
                 <ModalFooter>
                     <Button color="secondary left" onClick={() => this.props.toggleXLSModal()}>Cancel</Button>
